2649016b0fbc791593e7d56b3bb87a6d42ff61e4
Abrorbek0708 <abdumalikova972@gmail.comgit config --global user.name Abrorbek0708git config --global user.email abdumalikova972@gmail.com>
2025-12-25 16:08:06 +0500

    abror add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import Header from './components/Header'
-import Navbar from './components/Footer'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
+  
 
   return (
     <>
